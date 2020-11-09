6c8061f7d70c89659b9be850b4edca63f15ddb2e
maini <philip.mattsson@iths.se>
2020-11-09 19:49:08 +0100

    .

--- src/main/java/org/example/TextProcessor.java
+++ src/main/java/org/example/TextProcessor.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class TextProcessor {
 
+    static  StringBuilder str = new StringBuilder();
+    static  Scanner scan = new Scanner(System.in);
+
     static void textprocess(){
         
     }
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -6,4 +6,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 
+
+    @Test
+    void testAdd(){
+        Calculator calculator = new Calculator();
+        assertEquals(4, calculator.add(2,2));
+    }
+
+    @Test
+    void testSub(){
+        Calculator calculator = new Calculator();
+        assertEquals(0, calculator.sub(2, 2));
+    }
+
+    @Test
+    void testMulti(){
+        Calculator calculator = new Calculator();
+        assertEquals(8, calculator.multi(4, 2));
+    }
+
+    @Test
+    void testDiv(){
+        Calculator calculator = new Calculator();
+        assertEquals(4, calculator.div(8, 2));
+    }
 }
