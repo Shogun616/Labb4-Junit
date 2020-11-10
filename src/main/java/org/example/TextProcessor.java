9284030ba3eab7533686d1176be6d8e5916ba280
maini <philip.mattsson@iths.se>
2020-11-10 17:15:16 +0100

    Lagt till metoder för Textprocessor.

--- src/main/java/org/example/TextProcessor.java
+++ src/main/java/org/example/TextProcessor.java
@@ -2,22 +2,23 @@ package org.example;
 
 public class TextProcessor {
 
-    static  StringBuilder str = new StringBuilder();
-
-    public static void upperCase(){
-
+    public static void uppercase(){
+        System.out.println("java" + " verktyg");
     }
 
-    public static void common(){
-
+    public static void lowercase(){
+        System.out.println("java" + " verktyg");
     }
 
     public static void reverse(){
-
+        StringBuilder str = new StringBuilder("Java Verktyg");
+        str.reverse();
+        System.out.println(str);
     }
 
-
     public static void main(String[] args) {
-
+        uppercase();
+        lowercase();
+        reverse();
     }
 }
