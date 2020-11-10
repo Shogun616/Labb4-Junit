880e63425ef5edfb6285c2211a7b6797716cd9d3
maini <philip.mattsson@iths.se>
2020-11-10 11:41:56 +0100

    Version 2.5

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import java.util.Scanner;
-
 public class Calculator {
 
     public static int add(int a, int b) {
@@ -20,37 +18,10 @@ public class Calculator {
         return a / b;
     }
 
-    public static void calculate() {
-
-        Scanner scan = new Scanner(System.in);
-        int num1, num2;
-
-        System.out.println("Enter first number:");
-        num1 = scan.nextInt();
-        scan.nextLine();
-
-        System.out.println("Enter second number:");
-        num2 = scan.nextInt();
-        scan.nextLine();
-
-        System.out.println("Enter your selection: 1 for Addition, 2 for subtraction 3 for Multiplication and 4 for division:");
-        int choose;
-        choose = scan.nextInt();
-        switch (choose) {
-            case 1:
-                System.out.println(add(num1, num2));
-                break;
-            case 2:
-                System.out.println(sub(num1, num2));
-                break;
-            case 3:
-                System.out.println(multi(num1, num2));
-                break;
-            case 4:
-                System.out.println(div(num1, num2));
-                break;
-            default:
-                System.out.println("Invalid operation");
-        }
+    public static void main(String[] args) {
+        System.out.println("2 + 2 is " + add(2, 2) + ".");
+        System.out.println("2 - 2 is " + sub(2, 2) + ".");
+        System.out.println("4 * 2 is " + multi(4, 2) + ".");
+        System.out.println("8 / 2 is " + div(8, 2) + ".");
     }
 }
