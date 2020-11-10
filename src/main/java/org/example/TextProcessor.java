9fe809cd5a6065b6b0cb97c63ef3af0ab9192357
maini <philip.mattsson@iths.se>
2020-11-10 15:22:55 +0100

    Version 2.5

--- src/main/java/org/example/TextProcessor.java
+++ src/main/java/org/example/TextProcessor.java
@@ -7,7 +7,20 @@ public class TextProcessor {
     static  StringBuilder str = new StringBuilder();
     static  Scanner scan = new Scanner(System.in);
 
-    static void textprocess(){
-        
+    public static void upperCase(){
+
+    }
+
+    public static void common(){
+
+    }
+
+    public static void reverse(){
+
+    }
+
+
+    public static void main(String[] args) {
+
     }
 }
